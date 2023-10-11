f6e1cd229884ad37b13c398e380ae21bdf0637c4
Edi <146419637+Ediasecas@users.noreply.github.com>
2023-10-11 10:55:57 +0200

    IFS 11/10/23

---
+++ src/IFS.java
@@ -0,0 +1,9 @@
+
+public class IFS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ejercicio2ifs.java
@@ -0,0 +1,9 @@
+
+public class ejercicio2ifs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

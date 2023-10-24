c6cf645715be134e4edaeaba685f01b8d9c7aa44
Edi <146419637+Ediasecas@users.noreply.github.com>
2023-10-24 21:57:53 +0200

    ejercicio2
    actividad4

--- ud2acticidad4/ud2acticidad4/main.java
+++ ud2acticidad4/ud2acticidad4/main.java
@@ -181,7 +181,7 @@ public class main {
 		{
 			
 			numa[i] = numM;
-			System.out.println("El numero M en todas sus posiciones es " + numa[i]);
+			System.out.println("El numero M en  sus posicion " + i + " es " + numa[i]);
 			
 		}
 		
@@ -189,21 +189,288 @@ public class main {
 		
 		//EJERCICIO 7
 		
+		
+		int valoresenteros[]; //Valoresenteros[] es el array que almacena los valores introducidos P Y Q para comprobar quien es mayor
+		
 		System.out.println("Introduce el numero P ");
 		int numP = entrada.nextInt();
 		
 		System.out.println("Introduce el numero Q ");
 		int numQ = entrada.nextInt();
 		
-		int numc = Arrays.fill(numP,numQ);
+		int longitud;
 		
-		for (int i = 0 ;  i < numb.length ; i++)
 		
+		if (numP > numQ)
+			
 		{
 			
-			numb[numP] = numQ;
+				longitud = numP - numQ + 1; 
+				valoresenteros = new int[longitud]; 
+				
+				for (int i = numP; i >= numQ; i--) 
+	            
+				{
+	               
+	            valoresenteros[numP - i] = i; 
+	            System.out.println(valoresenteros[numP - i]+" , ");
+	            	
+	            } 
+	            
+	    } 
+	        
+		
+		if (numQ > numP) 
+		
+		{ 
+	            longitud = numQ - numP + 1; 
+	            valoresenteros = new int[longitud];
+	            
+	            for (int i = numP; i <= numQ; i++) 
+	            
+	            {
+	                
+	            valoresenteros[i - numP] = i;
+	            System.out.println(valoresenteros[i - numP]+" , ");
+	                
+	            }
+	    } 
+	       
+		
+		else 
+		
+		{ 
+	           
+				longitud = 1;
+	            valoresenteros = new int[longitud];
+	            valoresenteros[0] = numP;
+	            
+	    }
 			
-		}
+			
+		
+			//EJERCICIO 8
+		
+		
+			double numrandom [] = new double [100];
+		    double numR;
+		    int numreales= 0;
+
+		        for (int i=0; i < numrandom.length; i++) 
+		        
+		        {
+		            
+		        	numrandom [i] = Math.random();
+		        	
+		        }
+
+		    System.out.println("Escribe un numero real R, entre 0.0 y 1.0:");	  
+		    numR = entrada.nextDouble();
+
+		    for (int i = 0; i < numrandom.length; i++) 
+		    
+		    {
+		        
+		    	if (numR >= numrandom[i]) 
+		        
+		        {
+		           
+		        	numreales++;//Para que se muestren los numeros por encima del 0 hasta el numero introducido R
+		        	
+		        } 
+		    	
+		    } 
+
+		    System.out.println("Hay " + numreales + " numero/s mayores o iguales a " + numR  
+		    		+ " hasta llegar a 1.0");
+			
+	
+		    
+		    // EJERECICO 9
+
+			int nums100 [] = new int [100]; 
+			for (int i = 0; i < nums100.length ; i++)
+				
+			{
+				
+				nums100[i] = (int)(1 + Math.random()*10);
+				
+			}
+			
+			int valorN;
+			int contador1 = 0;
+			
+			System.out.println("Introduce el valor de N .");
+			valorN = entrada.nextInt();
+			
+			for (int i = 0; i < nums100.length ; i++) 
+				
+			{
+				if(nums100[i] == valorN) 
+					
+				{
+					contador1++;
+					System.out.println("Las posiciones de N = " + valorN + " son la " + i +  "º");
+					
+				}
+				
+			}
+			
+			if (contador1 == 0) 
+				
+			{
+			
+				System.out.println("No hay posiciones que contengan ese valor.");
+				
+			}
+
+	
+
+			//EJERCICIO 10
+			
+			int numalturas;
+			
+			System.out.println("Numero de alturas introducidas");
+			numalturas = entrada.nextInt();
+			
+			double alturamax = Integer.MIN_VALUE;
+			double alturamin = Integer.MAX_VALUE;
+			double sumh = 0;
+			
+			double alturas [] = new double [numalturas];
+			
+			for (int i = 0; i < alturas.length; i++)
+			
+			{
+				
+				double primerh;
+				
+				System.out.println("Ingresa una altura en metros");
+				primerh = entrada.nextDouble();
+				
+				if (primerh <= 0)
+				
+				{
+					
+					System.out.println("La altura ingresada no es válida");
+					
+				}
+				
+				alturas[i] = primerh;
+				
+			}
+			
+			for (int i = 0; i < alturas.length ; i++) 
+			
+			{
+				
+				if (alturas[i] > alturamax) 
+				
+				{
+					
+					alturamax = alturas[i];	
+					
+				}
+				
+				if (alturas[i] < alturamin) 
+				
+				{
+					
+					alturamin = alturas[i];	
+					
+				}
+				
+				sumh = sumh + alturas[i];
+				
+			}
+				
+			
+			System.out.println("La altura maxima es  " + alturamax + ", la altura minima es "
+			+ alturamin + " y la media de alturas es " + (sumh/alturas.length) + "." );	
+			
+			
+			
+			//EJERCICIO 11
+			
+			
+			int nument[] =new int[100];
+			int nument2[] =new int[100]; 
+			
+			System.out.println ("El primer array es "); 
+			
+			for(int i=0; i < nument.length; i++) 
+			
+			{
+				
+				nument[i] = i+1;
+				System.out.println (nument[i]);
+				
+			}
+			
+			System.out.println ("El segundo array es ");
+			
+			for(int i=0; i <nument2.length; i++) 
+			
+			{
+				
+				nument2[i] = nument[99 - i];
+				System.out.println (nument2[i]);
+				
+			}
+
+			
+			
+			
+			//EJERCICIO 14
+			
+			
+			int numerosrep[] = new int[100];
+			int valorinicial = 1; 
+			       
+		
+			for (int i = 0; i < 100; i++)//primer for para indicar que la cantidad de numeros va a ser 100
+
+        	{
+        	
+				for (int j = 0; j <= i/10; j++)//para indicar que los numeros tienen que ser 1,2 2,3 3 3... hasta llegar a 10
+        		
+				{
+			               
+					if (i < 100) 
+        		
+					{
+			                   
+        			numerosrep[i] = valorinicial;
+        			i++;
+			               
+					}
+			           
+				}
+
+        	valorinicial++;
+			       
+        	}
+			        
+			
+			for (int i = 0; i < 100; i++) 
+			       
+			{
+			   
+				System.out.print(numerosrep[i] + " ");
+				if ((i+1) % 10 == 0) 
+			
+				{
+			                
+				System.out.println();
+				
+				}
+			       
+			}
+			
+			
+			
+			
+		
 		
 		
 	}//cuerpo del ejerdda

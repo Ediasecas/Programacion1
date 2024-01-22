bd5975492de68c42e65bc9fde058786b77800c9d
Edi <146419637+Ediasecas@users.noreply.github.com>
2024-01-22 09:49:52 +0100

    Listas1

---
+++ Listas1/Main.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+	
+		ArrayList<Producto> products = new ArrayList<Producto>();
+		
+		
+		products.add(new Producto("Estuche",54));
+		products.add(new Producto("Ratón ",21));
+		products.add(new Producto("Monitor",64));
+		products.add(new Producto("Movil",21));
+		products.add(new Producto("Proyector",18));
+		
+		
+		
+		for(int i = 0; i < products.size(); i++) 
+		{
+			System.out.println(products.get(i).toString());
+		}
+		
+		
+		
+		Iterator<Producto> itera = products.iterator();
+		Producto products1;
+		while (itera.hasNext()) 
+		{
+			products1 = itera.next();
+			if(products1.getNombre().equals(Nombre))
+			{
+				itera.remove();
+			}
+			
+			System.out.println(products1.toString());
+		}
+		
+		
+		
+		
+		int posEliminar = -1;	
+		Producto pEliminar = null;
+		
+			for(Producto p : products)
+				{
+					if(p.getNombre().equals("72000031-A"))
+					{
+						posEliminar = products.indexOf(p);
+						pEliminar = p;
+					}
+					else {
+						System.out.println(p.toString());
+					}
+				}
+				
+				products.remove(pEliminar);
+		
+		
+	}//
+
+}//Clase 

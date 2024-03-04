c64b271b9caa784f6c540316296e03175d355700
Edi <146419637+Ediasecas@users.noreply.github.com>
2024-03-04 09:34:49 +0100

    Ficheros Unicode Terminado

--- FicherosUNICODE/Main.java
+++ FicherosUNICODE/Main.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	static LinkedList <Producto> miListaProductos = new LinkedList<Producto>();
+
 	
 	public static void main(String[] args) 
 	{
@@ -21,18 +21,20 @@ public class Main {
 		final String myPath = "./src";
 		final String myFile = "Almacen.dat";
 		
-		
+		LinkedList <Producto> miListaProductos = new LinkedList<Producto>();
 		
 		int seleccionar;
 		boolean salir = false;
 		
 		do 
 		{
-			System.out.println("1.Registrar Producto");
-			System.out.println("2.Mostrar Productos existente");	
-			System.out.println("3.Eliminar prducto por codigo");
-			System.out.println("4.Salir de la aplicacion");
+			System.out.println("1.Registrar producto");
+			System.out.println("2.Mostrar producto/s existente/s");	
+			System.out.println("3.Eliminar producto por codigo");
+			System.out.println("4.Guardar producto/s en el fichero");
+			System.out.println("5.Salir de la aplicacion");
 			System.out.println("Selceccione una opcion del menu escogiendola por su numero");
+			
 			seleccionar = entrada.nextInt();
 			
 			switch(seleccionar)
@@ -42,28 +44,73 @@ public class Main {
 					entrada = new Scanner(System.in);
 					
 					
-					System.out.println("Registrando producto");
+						System.out.println("Registrando producto");
+						
+						System.out.println("Introduzca el codigo");
+						String codigo = entrada.nextLine();
+						
+						System.out.println("Introduzca el nombre");
+						String nombre = entrada.nextLine();
+						
+						System.out.println("Introduzca la cantidad");
+						int cantidad = entrada.nextInt();
+						
+						System.out.println("Introduzca el precio");
+						double precio = entrada.nextDouble();
 					
-					System.out.println("Introduzca el codigo");
-					String codigo = entrada.nextLine();
-					
-					System.out.println("Introduzca el nombre");
-					String nombre = entrada.nextLine();
-					
-					System.out.println("Introduzca la cantidad");
-					int cantidad = entrada.nextInt();
-					
-					System.out.println("Introduzca el precio");
-					double precio = entrada.nextDouble();
 					
 					miListaProductos.add(new Producto(codigo, nombre, cantidad, precio));
+					
+					
+					
 				break;
 				
 				
 				
 				case 2:
 					
-					try(FileWriter myWriter = new FileWriter(myPath+myFile, false);
+					System.out.println("Mostrando productos existentes en el almacen");
+					for(Producto p: miListaProductos)
+					{
+						System.out.println(p.toString());
+					}
+					
+				break;
+				
+				
+				
+				case 3:
+					
+					entrada = new Scanner(System.in);
+					
+					System.out.println("Eliminando producto...");
+					
+					System.out.println("Introduzca el codigo del producto a borrar");
+					String codigoEliminar = entrada.nextLine();
+					
+					boolean productoEliminar = false;
+					for(int i =0; i < miListaProductos.size(); i++)
+					{
+						if(miListaProductos.get(i).getCodigo().equals(codigoEliminar))
+						{
+							miListaProductos.remove(i);
+							productoEliminar = true;
+						}
+					}
+					
+					if(productoEliminar = true)
+					{
+						System.out.println("Producto con codigo " + codigoEliminar + " eliminado correctamente");
+						
+					}
+					
+					
+				break;	
+				
+				
+				case 4:
+					
+                    try(FileWriter myWriter = new FileWriter(myPath+myFile, false);
 							BufferedWriter buffer = new BufferedWriter(myWriter);)
 					{
 						for(Producto producto : miListaProductos)
@@ -85,37 +132,23 @@ public class Main {
 					{
 						System.out.println("La escritura ha finalizado con exito.");
 					}
-			
+					
 				break;
 				
 				
-				
-				case 3:
-					
-					entrada = new Scanner(System.in);
-					
-					System.out.println("Eliminando producto...");
-					
-					System.out.println("Introduzca el codigo del producto a borrar");
-					String codigoEliminar = entrada.nextLine();
-					
-					eliminarProducto(codigoEliminar);
-					
-					
-				break;	
-				
-				
-				case 4:
+				case 5:
 					
 					entrada = new Scanner(System.in);
 					
 					System.out.println("Usted salió del programa correctamente");
                     salir = true;
-					
+                    
 				break;
+				
 			}
 	
 		}
+		
 		while(!salir);
 		
 		
@@ -124,22 +157,6 @@ public class Main {
 	}//main
 	
 
-	public static void eliminarProducto(String codigoEliminar)
-	{
-		
-			if(codigoEliminar != null)
-			{
-				miListaProductos.remove(codigoEliminar);
-			}
-			
-			else
-			{
-				System.out.println("Esta codigo no se encuentra en el la lista de productos");
-			}	
-			
-	}
-	
-	
 	
 }//clase	
 	

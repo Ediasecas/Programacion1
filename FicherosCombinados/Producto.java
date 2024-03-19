78fbdbe90408ec844f741614f7a0580179445667
Edi <146419637+Ediasecas@users.noreply.github.com>
2024-03-19 17:06:44 +0100

    cxd

--- FicherosCombinados/Producto.java
+++ FicherosCombinados/Producto.java
@@ -2,8 +2,10 @@
 public class Producto
 {
 	private String referencia;
-	private String Descripción;
-	private int Cantidad;
+	private String descripcion;
+	private String tipo;
+	private int cantidad;
+	private double precio ;
 	private int descuento ;
 	private int IVA;
 	private boolean aplicarDto;

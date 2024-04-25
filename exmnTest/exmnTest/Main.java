add513b08e93ac16bbb1e5d9055c9040a8d23b72
Edi <146419637+Ediasecas@users.noreply.github.com>
2024-04-25 13:28:25 +0200

    SS

---
+++ exmnTest/exmnTest/Main.java
@@ -0,0 +1,20 @@
+package exmnTest;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		
+	
+
+		 String nombre = "Pepe";
+
+		 String apellido = "Martinez";
+
+		 String todo = nombre + " " + apellido;
+
+		 System.out.println (todo);
+		 
+	}
+
+}

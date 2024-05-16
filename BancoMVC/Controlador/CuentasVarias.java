345e0d23cd06845a03129383d51028f8139d1cea
Edi <146419637+Ediasecas@users.noreply.github.com>
2024-05-16 11:35:48 +0200

    BancoMVC entrega final

--- BancoMVC/Controlador/CuentasVarias.java
+++ BancoMVC/Controlador/CuentasVarias.java
@@ -12,33 +12,54 @@ import Modelo.Cuenta;
 
 public class CuentasVarias
 {
-	
-	final boolean modoApend = true;
+	//Clase CuentasVarias donde podremos encontrar los metodos.
     
-	private List<Cuenta> cuentasBancarias = new LinkedList<>();
+	private List<Cuenta> cuentasBancarias = new LinkedList<>();/*   Creacion de una coleccion de cuentas para poder trabajar 
+																 	sobre ellas, en este caso la coleccion es LinkedList   */
 
 	
-	public void CrearCuenta(Cuenta c)
-	{	
-		cuentasBancarias.add(c); 
-	    System.out.println("Cuenta registrada con éxito.");
+	 public void crearCuenta(Cuenta c) //Metodo crearCuenta objeto nuevo de Cuenta = c
+	 {
+        // Verificar si el código identificativo ya está en uso
+        if (buscarCuentaPorCodigo(c.getCod()) == null) 
+        {
+            cuentasBancarias.add(c);//Añadimos a nuestra coleccion cuentasBancarias nuestra cuenta c
+            System.out.println("Cuenta registrada con éxito.");
+        } 
+        else//Si no se cumple la condicion que al buscarCuentaPorCodigo = null mandará un msg de que ya existe una cuenta con ese cod por lo tanto no la creará.
+        {
+            System.out.println("Ya existe una cuenta con este código identificativo. No se pudo crear la cuenta.");
+        }
+    }
+	 
+	 
+	private Cuenta buscarCuentaPorCodigo(String codigo) //Creacion del metodo buscarCuentaPorCodigo.
+	{
+		for (Cuenta cuenta : cuentasBancarias) //Iteramos entre cuentas para poder buscar Cuentas
+	    {
+			if (cuenta.getCod().equals(codigo)) //Si el codigo creado introducido coincide devolvemos cuenta
+	        {
+	           return cuenta;
+	        }
+	    }
+	  return null; 
 	}
-	
+
 	
 	public void leerCuenta(String codigo)
 	{
-		for(Cuenta cuenta : cuentasBancarias)
+		for(Cuenta cuenta : cuentasBancarias)//Iteramos entre nuestra coleccion de cuentas buscando los obj cuenta
 		{
-			if(cuenta.getCod().equals(codigo))
+			if(cuenta.getCod().equals(codigo))//Si el codigo de cuenta introducido en el main coincide con el codigo de una cuenta creada de nuestra coleccion hace la siguiente funcion
 			{
-				System.out.println(cuenta.toString());
+				System.out.println(cuenta.toString());//En este caso escribe el toString
 			}
 			else
 			{
 				System.out.println("No se encontró el codigo de esa cuenta");
 			}
 		}
-		if(cuentasBancarias.size() == 0)
+		if(cuentasBancarias.size() == 0)//Si no existe ninguna cuenta saldra el msg posterior, para que tengamos que crear una cuenta para poder leer,borrar o acutalizar cuentas.
 		{
 			System.out.println("No hay cuentas agregadas en el banco en este momento");
 		}
@@ -48,11 +69,11 @@ public class CuentasVarias
 	public void eliminarCuenta(String codigo)
 	{
 		
-		for(Cuenta cuenta : cuentasBancarias)
+		for(Cuenta cuenta : cuentasBancarias)//Iteramos entre nuestra coleccion de cuentas buscando los obj cuenta.
 		{
-			if(cuenta.getCod().equals(codigo))
+			if(cuenta.getCod().equals(codigo))//Si el codigo de cuenta introducido en el main coincide con el codigo de una cuenta creada de nuestra coleccion hace la siguiente funcion
 			{
-				cuentasBancarias.remove(cuenta);
+				cuentasBancarias.remove(cuenta);//En este caso se elimina la cuenta de la coleccion
 				System.out.println("La cuenta con codigo " + codigo + " se ha eliminado correctamente");
 			}
 			else
@@ -61,7 +82,7 @@ public class CuentasVarias
 			}
 		}
 		
-		if(cuentasBancarias.size() == 0)
+		if(cuentasBancarias.size() == 0)//Si no existe ninguna cuenta saldra el msg posterior, para que tengamos que crear una cuenta para poder leer,borrar o acutalizar cuentas.
 		{
 			System.out.println("No hay cuentas agregadas en el banco en este momento");
 		}
@@ -69,15 +90,16 @@ public class CuentasVarias
 	}
 	
 	
-	public void actualizarCuenta(String codigo, String nuevoNombre, String nuevoNumero, double nuevoSaldo) 
+	public void actualizarCuenta(String codigo, String nuevoNombre, String nuevoNumero, double nuevoSaldo) //Creción del metodo actualizarCuenta pidiendo los datos cod, nuevoNombre...
 	{
-		for (Cuenta cuenta : cuentasBancarias) 
+		for (Cuenta cuenta : cuentasBancarias)//Iteramos entre nuestra coleccion de cuentas buscando los obj cuenta.
 		{
-			if (cuenta.getCod().equals(codigo)) 
+			if (cuenta.getCod().equals(codigo))//Si el codigo de cuenta introducido en el main coincide con el codigo de una cuenta creada de nuestra coleccion hace la siguiente funcion.
 			{
 				cuenta.setNombreCuenta(nuevoNombre);
 	            cuenta.setNumCuenta(nuevoNumero);
 	            cuenta.setSaldo(nuevoSaldo);
+	            //En este caso modificamos con los nuevos datos introducidos los datos anteriormente creados mediante cuenta.setAtributo(nuevoAtributo)
 	            
 	            System.out.println("Los datos de la cuenta con codigo " + codigo + " se han actualizado correctamente.");
 	            return; 
@@ -93,10 +115,10 @@ public class CuentasVarias
 			System.out.println("No hay cuentas agregadas en el banco en este momento");
 		}
 	      
-	 }
+	}
 	
 	
-	public void guardarCuenta()
+	public void guardarCuenta()//Metodo guardar cuenta
 	{
 		String filePath = "./modelo/Cuentas.dat";
 		try (FileOutputStream os = new FileOutputStream(filePath);
--- BancoMVC/Vista/VentanaPrincipal.java
+++ BancoMVC/Vista/VentanaPrincipal.java
@@ -1,6 +1,7 @@
 package Vista;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import Controlador.CuentasVarias;
 import Modelo.Cuenta;
@@ -13,15 +14,15 @@ public class VentanaPrincipal {
 			Scanner entrada = new Scanner(System.in);
 			System.out.println("__ Banco Santander __ ");
 				
-			CuentasVarias banco = new CuentasVarias();
+			CuentasVarias banco = new CuentasVarias();//Creacion del banco para poder trabajar sobre él.
 				
-				
-     
-			boolean salir = false;
-			int seleccionar = 0;
+
+			boolean salir = false; //Inicializamos la variable salir = false para el case 6
+			int seleccionar = 0;//creacion de la variable para "seleccionar" para poder escoger una opcion del menu.
 				
 				do 
 				{
+					//menu de opciones de nuestro banco
 					
 			        System.out.println("1. Crear Cuenta");
 			        System.out.println("2. Leer datos de Cuenta");
@@ -29,45 +30,70 @@ public class VentanaPrincipal {
 			        System.out.println("4. Borrar Cuenta");
 			        System.out.println("5. Guardar datos de Cuenta");
 			        System.out.println("6. Salir");
-			        System.out.println("Selceccione una opcion del menu escogiendola por su numero");
+			        System.out.println("Selceccione una opción del menu escogiendola por su número");
 			        
 			       
 			        seleccionar = entrada.nextInt();
-					switch(seleccionar)
+					switch(seleccionar)//switch para intercalar entre opciones escogidas por su numero
 					
 					{
 					
-						case 1:				
-						        
-							System.out.println("Introduzca el codigo idntificativo de la cuenta ");
-							String cod = entrada.next();
-							     
-							    
+						case 1:			//Creacion de la cuenta	
 							entrada.nextLine();
-							System.out.println("Introduzca el nombre del titular la cuenta");	    
-							String nombreCuenta = entrada.nextLine();
+							
+						    System.out.println("Introduzca el código identificativo de la cuenta (Formato: BANK_XXX):");
+			                String cod = "";
+			                boolean codigoValido = false;
+
+			                while (!codigoValido)
+			                {
+			                 cod = entrada.nextLine();
+			                  if (Pattern.matches("BANK_\\d{3}", cod)) 
+			                  {
+			              	  codigoValido = true;
+			                  } 
+			                  else 
+			                  {
+			                	  System.out.println("Formato incorrecto. Introduzca un código válido (BANK_XXX):");
+			                  }
+			                }//validador del codigo identificativo, tiene que cumplir el formato BANK_123  O BANK_000 el (varia el numero) 
+			                     
+							    
+							
+			                System.out.println("Introduzca el nombre del titular la cuenta:");//Solicitud del nombre del titular de la cuenta sin formato.  
+			                String nombreCuenta = entrada.nextLine();
 
 							       
+			                System.out.println("Introduzca el numero de cuenta (Ejemplo: 1234 1234 XX 1234567890):");
+			                String numCuenta;
+
+			                while (true) 
+			                {
+			                	numCuenta = entrada.nextLine();
+			                	if (Pattern.matches("\\d{4} \\d{4} [A-Z]{2} \\d{10}", numCuenta)) 
+			                	{
+			                		break; 
+			                	} 
+			                	else 
+			                	{
+			                		System.out.println("Formato incorrecto. Introduzca un número de cuenta válido (Ejemplo: 1234 1234 XX 1234567890):");
+			                	}
+			                }//validador del numero de cuenta, tiene que cumplir el formato 1234 1234 EE 1234567890  (Pueden variar tanto nums como letras XX pero cumpliendo con el formato de los espacios y length) 
 							
-							System.out.println("Introduzca el numero de cuenta");
-							String numCuenta = entrada.nextLine();
+			                Cuenta nuevaCuenta = new Cuenta(cod, nombreCuenta, numCuenta, 0);//Creacion de la cuenta (creacion del objeto) pasandole los datos pedidos anteriormente, al crear cuenta pondremos el saldo como 0 ya que es una cuenta nueva
 							
-							Cuenta nuevaCuenta = new Cuenta(cod, nombreCuenta, numCuenta, 0);
-							
-							banco.CrearCuenta(nuevaCuenta);
+			                banco.crearCuenta(nuevaCuenta);//Pasamos a nuestro banco el metodo crear cuenta para pasarle nuestro nueva cuenta y que quede guardada
 							
 						break;
 						
 						
 						case 2:
-							entrada.nextLine();
+							entrada.nextLine(); 
 							
-							System.out.println("Introduzca el codigo identificativo de la cuenta que desea leer sus datos");
-							String codigoL = entrada.nextLine();
-							entrada.nextLine();
+							System.out.println("Introduzca el código identificativo de la cuenta que desea leer sus datos (Formato: BANK_XXX):");
+							String codigoL = entrada.nextLine();//Pedimos el codigo de la cuenta que deseemos leer sus datos, el codigo cumplira con el mismo formato ya que esta pedido y guardado al crear cuenta.
 							
-							banco.leerCuenta(codigoL);
-							
+							banco.leerCuenta(codigoL);//Llamamos al metodo leerCuenta de la cuenta con el codigo pedido anteriormente (codigoL)
 							
 						break;
 						
@@ -75,21 +101,35 @@ public class VentanaPrincipal {
 						case 3:
 							entrada.nextLine();
 							
-							System.out.println("Introduzca el código identificativo de la cuenta que desea actualizar:");
+							System.out.println("Introduzca el código identificativo de la cuenta que desea actualizar (Formato: BANK_XXX):");//Pedimos el codigo de la cuenta que deseemos actualizar, el codigo cumplira con el mismo formato ya que esta pedido y guardado al crear cuenta.
 			                String codigoA = entrada.nextLine();
 			                
-			                System.out.println("Introduzca el nuevo nombre del titular de la cuenta:");
+			                System.out.println("Introduzca el nuevo nombre del titular de la cuenta:");//Pedimos otra vez los datos de la cuenta, en este caso el nombre del titular de la cuenta.
 			                String nuevoNombre = entrada.nextLine();
 			                
-			                System.out.println("Introduzca el nuevo número de cuenta:");
+			                System.out.println("Introduzca el nuevo número de cuenta (Ejemplo: 1234 1234 XX 1234567890)");/*Pedimos el numero de cuenta, en este caso
+			                																								tendremos que pedir el formato otra vez ya que 
+			                																								no es como el codigo identificativo que es 
+			                																								el mimso para todos, ya que al actualizar los datos podría cambiar el formato*/
 			                String nuevoNumero = entrada.nextLine();
 			                
+			                while (true) 
+		                    {
+		                        nuevoNumero = entrada.nextLine();
+		                        if (Pattern.matches("\\d{4} \\d{4} [A-Z]{2} \\d{10}", nuevoNumero)) 
+		                        {
+		                            break; 
+		                        } 
+		                        else 
+		                        {
+		                            System.out.println("Formato incorrecto. Introduzca un numero de cuenta válido (Ejemplo: 1234 1234 XX 1234567890):");
+		                        }
+		                    }//Patern del formato
+			                
 			                System.out.println("Introduzca el nuevo saldo de la cuenta:");
-		                    double nuevoSaldo = entrada.nextDouble();
-		                    
-		                    entrada.nextLine();
-		                    
-			                banco.actualizarCuenta(codigoA, nuevoNombre, nuevoNumero, nuevoSaldo);
+		                    double nuevoSaldo = entrada.nextDouble();//Al actualizar los datos de cuenta pedirá tambien el saldo .
+		                                    
+			                banco.actualizarCuenta(codigoA, nuevoNombre, nuevoNumero, nuevoSaldo);//Llamamos al metodo actualizarCuenta y pasamos los nuevos datos introducidos.
 			                    
 	
 						break;	
@@ -98,17 +138,17 @@ public class VentanaPrincipal {
 						case 4:
 							entrada.nextLine();
 							
-							System.out.println("Introduzca el codigo identiificativo de la cuenta que desea borrar");
+							System.out.println("Introduzca el codigo identificativo de la cuenta que desea borrar (Formato: BANK_XXX)");//Pedimos el cod para poder eliminar cuenta.
 							String codigoB = entrada.nextLine();
-							entrada.nextLine();
-							
-							banco.eliminarCuenta(codigoB);
+					
+							banco.eliminarCuenta(codigoB);//Llamamos al metodo eliminarCuenta de la cuenta con el codigo pedido anteriormente (codigoB).
 						break;
 						
 						
 						case 5:
 							
-							 banco.guardarCuenta(); 
+							 banco.guardarCuenta();/*Guardamos todas las cuentas, aunque borremos una cuenta y abramos el archivo cuenta.dat si
+							 no le damos a guardar cuenta no se actualizar los datos en cuenta.dat*/
 			                
 						break;
 
@@ -116,7 +156,7 @@ public class VentanaPrincipal {
 						case 6:
 						   
 							 System.out.println("Usted salio del programa correctamente");
-			                 salir = true;
+			                 salir = true;//Declaramos salir = true para poder salir del menu.
 						    
 						break;
 							
@@ -124,7 +164,7 @@ public class VentanaPrincipal {
 							
 					
 							
-					}//llave del switch
+					}//llave del Switch.
 				
 				
 				}while(!salir);
